a8f9a62d0446aed7951e2f2b71ead98f2d9611ea
Camilo Mason <camilomason@Camilos-MBP.home>
2021-03-23 23:06:40 -0400

    removing sendgrid api key lol
    can't believe I did that

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'camzillajs',
-  :password => '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',
-  :domain => 'sezmereviews.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'camzillajs',
+#   :password => '',
+#   :domain => 'sezmereviews.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
