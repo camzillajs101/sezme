ab6bb26e5d4c5a7dccd2949410a8af900ce75474
Camilo Mason <camilomason0@gmail.com>
2021-04-04 23:34:13 -0400

    fixing it yet again?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,8 +114,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
-    :password => Rails.application.credentials.dig(:sendgrid, :password),
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
