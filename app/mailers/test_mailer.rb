a6145084457089eaba89fd6743d87018bd53aab6
Camilo Mason <camilomason0@gmail.com>
2021-04-05 11:49:10 -0400

    Adding a bunch of features to devise user

--- app/mailers/test_mailer.rb
+++
@@ -1,5 +0,0 @@
-class TestMailer < ApplicationMailer
-  def hello
-    mail(to: "camilomason0@gmail.com", subject: 'Welcome to My Awesome Site')
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :timeoutable and :omniauthable
 
   has_many :posts, dependent: :destroy
   has_many :reviews
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :username, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
 end
