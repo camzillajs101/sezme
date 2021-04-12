5c5cd7ea348e507e5d3ede65228045b218c2ce00
Camilo Mason <camilomason0@gmail.com>
2021-04-12 10:15:50 -0400

    Adding tag_list input to edit form as well

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :desc, :rating)
+      params.require(:post).permit(:title, :desc, :rating, :tag_list)
     end
 end
