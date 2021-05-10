186b1cf3873d79fd1bd2b627e5c3e40a5c28630e
Camilo Mason <camilomason0@gmail.com>
2021-05-10 18:13:55 -0400

    setting branch to main instead of master
    github, why you gotta be like that?

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :branch, proc { `git rev-parse --abbrev-ref main`.chomp }
 
 # role-based syntax
 # ==================
