8f64b109d7b65641befaae4d4ae08a3bc97bb15c
Camilo Mason <camilomason@Camilos-MacBook-Pro.local>
2021-06-29 00:31:02 -0400

    Posts now calculate rating at request rather than aggregate

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,26 @@ class Post < ApplicationRecord
   has_many :reviews
 
   validates :title, :desc, presence: true
+
+  def self.rating(id)
+    reviews = Post.find(id).reviews
+    count = [reviews.count, 1].max
+    sum = 0
+
+    reviews.each do |review|
+      sum += review.rating
+    end
+
+    return sum / count / 10.0
+  end
+
+  def self.readablerating(id)
+    rating = Post.rating(id)
+
+    if rating < 1
+      return "[unrated]"
+    else
+      return "#{rating} stars"
+    end
+  end
 end
