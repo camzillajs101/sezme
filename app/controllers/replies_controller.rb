6cfa83a23bbbb642c88cacc16f7d26f7a44dba45
Camilo Mason <camilomason0@gmail.com>
2021-06-30 15:52:16 -0400

    New reply functionality
    Happy with this code

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,22 @@
 class RepliesController < ApplicationController
+  def new
+    @review = Review.find(params[:review_id])
+    @reply = @review.replies.new
+  end
+
+  def create
+    @review = Review.find(params[:review_id])
+    @reply = @review.replies.new(reply_params)
+
+    if @reply.save
+      redirect_to @review
+    else
+      render :new
+    end
+  end
+
+  protected
+    def reply_params
+      params.require(:reply).permit(:body).merge(user_id: current_user.id)
+    end
 end
